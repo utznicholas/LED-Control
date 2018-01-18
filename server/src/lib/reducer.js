f550b235855df26326b75d43bbc658e43ad3e439
utznicholas <utznicholas@gmail.com>
2018-01-18 16:07:10 -0500

    Added action subscription functionality to root reducer

--- server/src/lib/reducer.js
+++ server/src/lib/reducer.js
@@ -11,41 +11,54 @@ import * as Timelines from '/lib/reducers/timelines';
 import * as Schedules from '/lib/reducers/schedules';
 import * as Instances from '/lib/reducers/instances';
 
+import EventEmitter from 'events';
+
 export function setStore(str) {
   Schedules.setStore(str);
   Instances.setStore(str);
 }
 
-export default function reduce(state = {}, action = {}) {
-  let newState;
-  if (action.type == Actions.BATCH_ACTION) {
-    newState = Object.assign({}, state, {
-      lastAction: Actions.BATCH_ACTION
-    });
-    let actions = action.actions;
-    for (let i = 0; i < actions.length; i++) {
-      let act = actions[i];
+export default class Reducer extends EventEmitter {
+  constructor() {
+    
+  }
+  
+  reduce(state = {}, action = {}) {
+    let newState;
+    if (action.type == Actions.BATCH_ACTION) {
+      newState = Object.assign({}, state, {
+        lastAction: Actions.BATCH_ACTION
+      });
+      let actions = action.actions;
+      for (let i = 0; i < actions.length; i++) {
+        let act = actions[i];
+        newState = {
+          devices: Devices.reduce(state.devices, act),
+          channels: Channels.reduce(state.channels, act),
+          colors: Colors.reduce(state.colors, act),
+          timelines: Timelines.reduce(state.timelines, act),
+          schedules: Schedules.reduce(state.schedules, act),
+          instances: Instances.reduce(state.instances, act)
+        };
+      }
+
+    } else {
       newState = {
-        devices: Devices.reduce(state.devices, act),
-        channels: Channels.reduce(state.channels, act),
-        colors: Colors.reduce(state.colors, act),
-        timelines: Timelines.reduce(state.timelines, act),
-        schedules: Schedules.reduce(state.schedules, act),
-        instances: Instances.reduce(state.instances, act)
+        lastAction: action.type,
+        devices: Devices.reduce(state.devices, action),
+        channels: Channels.reduce(state.channels, action),
+        colors: Colors.reduce(state.colors, action),
+        timelines: Timelines.reduce(state.timelines, action),
+        schedules: Schedules.reduce(state.schedules, action),
+        instances: Instances.reduce(state.instances, action)
       };
     }
     
-  } else {
-    newState = {
-      lastAction: action.type,
-      devices: Devices.reduce(state.devices, action),
-      channels: Channels.reduce(state.channels, action),
-      colors: Colors.reduce(state.colors, action),
-      timelines: Timelines.reduce(state.timelines, action),
-      schedules: Schedules.reduce(state.schedules, action),
-      instances: Instances.reduce(state.instances, action)
-    };
+    setImmediate(() => {
+      this.emit("action", action);
+      this.emit(action.type, action);
+    });
+    
+    return newState;
   }
-  
-  return newState;
 }
